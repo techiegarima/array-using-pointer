c71b8991679737a850ead3697a70b2154a4c58dc
techiegarima <80690565+techiegarima@users.noreply.github.com>
2022-02-28 11:40:20 +0530

    array using pointer.cpp

---
+++ array pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+   int arr[5];
+   int *ptr=arr;
+    cout<<"Enter elements:"
+    for(i=0;i<5i++)
+    cin>>ptr+i;
+    cout<<"You entered:"
+    for(i=0;i<5;i++
+    cout<<&(ptr+i)<<endl;
+    return 0;
+ }
+    
+    
